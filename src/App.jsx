e0ee215e68841023768ddcc9b1c0177fb50bbe7f
Abeni <abenezeryirgalem0@gmail.com>
2025-04-08 00:53:38 +0300

    build Home page with responsive layout including hero section, testimonial carousel, partner showcase, and footer with subscription form

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import HeroSection from "./pages/HeroSection";
+import Testimonials from "./pages/Testimonials";
+import Navbar from "./components/Navbar";
+import Content1 from "./pages/Content1";
+import Content2 from "./pages/Content2";
+import Partners from "./pages/Partners";
+import Footer from "./pages/Footer";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <div>
+     <Home />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import HeroSection from "./HeroSection";
+import Content1 from "./Content1";
+import Content2 from "./Content2";
+import Partners from "./Partners";
+import Testimonials from "./Testimonials";
+import Footer from "./Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+      <Content1 />
+      <Content2 />
+      <Partners />
+      <Testimonials />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
